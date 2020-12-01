9ab854268339b07815daa0569f3347b12d6824c2
andreamaraldeoliveira <andreamaraldeoliveira@localhost>
2020-12-01 16:50:52 +0000

    Iniciar regra longMethod()

--- ProjetoES/src/main/java/data_service/Rules/Rules.java
+++ ProjetoES/src/main/java/data_service/Rules/Rules.java
@@ -1,5 +1,28 @@
 package data_service.Rules;
 
 public class Rules {
+	
+	public boolean logMethod(int loc, int cyclo, int thresholds){
+		if(loc + cyclo > thresholds) {
+			return true;
+		}
+		return false;
+		
+	}
+	
+	
+	
+	
+	
 
 }
+
+
+//para o log-method é criar um metodo que recebe como argumentos (o int LOC, 
+//o int CYCLO e os treshholds para ser considerado codesmell) e se os 
+//valores das metricas for maior que os treshholds retornar true.
+
+//o metodo tem de conseguir lidar com o facto que a regra pode ser 
+//'LOC AND CYCLO', 'LOC OR CYCLO' ou pode só ter um dos valores dados, 
+//logo provavelmente precisas de mais um argumento para saber se é para 
+//aplicar o operador 'AND' ou 'OR'.
