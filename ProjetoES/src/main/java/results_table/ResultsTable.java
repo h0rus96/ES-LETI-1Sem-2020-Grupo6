023bc73af5c2546b53b58eb3f6ef003ba5099902
pcmee-iscte <72449457+pcmee-iscte@users.noreply.github.com>
2020-12-02 18:49:31 +0000

    Results Table Finished

--- ProjetoES/src/main/java/results_table/ResultsTable.java
+++ ProjetoES/src/main/java/results_table/ResultsTable.java
@@ -14,15 +14,42 @@ public class ResultsTable {
 	private JPanel panel = new JPanel();
 	private String[] columns = {"Indicador", "iPlasma", "PMD", "Custom_Rules"};
 	private String[] lines = {"DCI", "DII", "ADCI", "ADII"};
-	private DefaultTableModel model;
+	private int[][] resultsList;
+	private DefaultTableModel model = new DefaultTableModel(columns, 4);;
 	
-	public ResultsTable() {
+	public ResultsTable(int[] resultsList) {
+		this.resultsList = convertTo2DMatrix(resultsList);
 		
 	}
 	
+	
+	private int[][] convertTo2DMatrix(int[] resultsList2) {
+		int[][] matrix = new int[3][4];
+		int counter=0;
+		
+        for(int i=0;i<3;i++) {
+        	
+            for(int j=0;j<4;j++) {
+            	
+                if(counter==resultsList2.length) break;
+
+                matrix[i][j]=resultsList2[counter];
+
+            System.out.printf("matrix[%d][%d]= %d\n",i,j,matrix[i][j]);
+
+            counter++;
+
+            }
+
+        }
+		
+		return matrix;
+	}
+
+	
 	private void showTable() {
 		panel.setLayout(new BorderLayout());
-		JFrame frame = new JFrame();
+		JFrame frame = new JFrame("Results Table");
 		panel.add(table);
 		JScrollPane js=new JScrollPane(table);
 		js.setVisible(true);
@@ -37,24 +64,28 @@ public class ResultsTable {
 	//Falta Indicadores
 	private void addResults() {
 		for(int row = 0; row != model.getRowCount(); row++) {
-			System.out.println(lines[row]);
+			
+			System.out.println(lines[row]);			
 			model.setValueAt(lines[row], row, 0);
+			
 			for(int column = 1; column != model.getRowCount(); column++) {
-				model.setValueAt(row*10, row, column);
+				
+				model.setValueAt(resultsList[column-1][row], row, column);
+				
 			}
 		}
+		
 	}
 	
 	public void buildTable() {
-		model = new DefaultTableModel(columns, 4);
 		addResults();
-		table = new JTable(model);
+		table = new JTable(model);		
 		showTable();
 	}
 	
 	
 	public static void main(String[] args)  {
-		ResultsTable resultstable = new ResultsTable();
+		ResultsTable resultstable = new ResultsTable(new int[]{0,1,2,3,10,11,21,31,20,12,22,32,30,13,23,33});
 		resultstable.buildTable();
 	}
 }
